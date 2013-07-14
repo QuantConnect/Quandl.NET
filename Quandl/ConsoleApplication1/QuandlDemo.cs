e68ba2498e0745b27ea30cdba640f18fb37d23c9
Ryan Hill <ryane.hill@gmail.com>
2013-07-14 17:11:35 +1200

    Commit of the first code
    Added base code for QuandlDotNet namespace and Quandl class.
    
    Added basic getDataSet function, for accessing and downloading data from
    Quandl.com

---
+++ Quandl/ConsoleApplication1/QuandlDemo.cs
@@ -0,0 +1,41 @@
+﻿/*
+ * Created by Ryan Hill, Copyright July 2013
+ * 
+ *  This file is part of QuandlDotNet package.
+ * 
+ *  QuandlDotNet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  QuandlDotNet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with QuandlDotNet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using QuandlDotNet;
+
+namespace QuandlDotNet
+{
+    class QuandlDemo
+    {
+        static void Main(string[] args)
+        {
+            Quandl myQuandl = new Quandl();
+            Dictionary<string, string> kwargsDemo = new Dictionary<string,string>();
+            kwargsDemo.Add("collapse", "weekly");
+            kwargsDemo.Add("trim_start", "2010-02-01");
+            kwargsDemo.Add("trim_end", "2010-03-28");
+            myQuandl.getDataSet("GOOG/NYSE_IBM", kwargsDemo);
+            myQuandl.writeToDataFile("testData");
+        }
+    }
+}
---
+++ Quandl/Quandl/QuandlDotNet.cs
@@ -0,0 +1,105 @@
+﻿/*
+ * Created by Ryan Hill, Copyright July 2013
+ * 
+ *  This file is part of QuandlDotNet package.
+ * 
+ *  QuandlDotNet is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ * 
+ *  QuandlDotNet is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with QuandlDotNet.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+
+namespace QuandlDotNet
+{
+
+    public class Quandl
+    {
+        /* Wrapper class for accessing the Quandl.com API
+         */
+        private string QUANDL_API_URL = "http://www.quandl.com/api/v1/";
+        private string authorToken;
+        private string data;
+        private string outputFormat;
+
+        public Quandl(string authortoken = "")
+        {
+            
+            /* Constructor
+             * if authortoken not specified on construction then access is limited to 10 per day
+             */
+            authorToken = authortoken;
+        }
+
+        public void setAuthorToken(string token)
+        {
+            /* Used to set AuthorToken if you forgot to specify at construction
+             */
+            authorToken = token;
+        }
+
+        public void getDataSet(string dataset, Dictionary<string, string> kwargs, string format = "csv")
+        {
+            /* Princple function for getting data about a give stock 
+             * dataset = dataset code as per Quandl.com website
+             * format = format for data to be returned as, default = "csv". Options are "csv", "plain", "json", "xml"
+             * 
+             * kwargs = A dictionary of keyords describing what to data to obtain as follows:
+             *  trim_start: format is "yyyy-mm-dd"
+             *  trim_end: format is "yyyy-mm-dd"
+             *  collapse: Options are "daily", "weekly", "monthly", "quarterly", "annual"
+             *  transformation: options are "diff", "rdiff", "cumul", and "normalize"
+             *  rows: Number of rows which will be returned, e.g. ("rows", "1")
+             *  sort_order: options are "asc", "desc". Default: "asc"
+             *  
+             *  In addition any other Quandl.com parameter can be passed
+             */
+            outputFormat = format;
+            string requestUrl;
+            if (authorToken == "")
+            {
+                requestUrl = QUANDL_API_URL + String.Format("datasets/{0}.{1}?", dataset, format);
+                foreach (KeyValuePair<string, string> kvp in kwargs)
+                {
+                    requestUrl = requestUrl + String.Format("{0}={1}&", kvp.Key, kvp.Value);
+                }
+            }
+            else
+            {
+                requestUrl = QUANDL_API_URL + String.Format("datasets/{0}.{1}?auth_token={2}", dataset, format, authorToken);
+                foreach (KeyValuePair<string, string> kvp in kwargs)
+                {
+                    requestUrl = requestUrl + String.Format("&{0}={1}", kvp.Key, kvp.Value);
+                }
+            }
+
+            WebClient client = new WebClient();
+            data = client.DownloadString(requestUrl);
+
+        }
+
+        public void writeToDataFile(string fileName)
+        {
+            /* For debug purposes only
+             */
+            using (StreamWriter outfile = new StreamWriter(fileName + "." + outputFormat))
+            {
+                outfile.Write(data);
+            }
+        }
+    }
+}
